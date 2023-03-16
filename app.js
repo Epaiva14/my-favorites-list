f6abd3d6c5801edc79472dddc554ddb32eb20a3f
Alexis Laronga <alexislaronga@Alexiss-MacBook-Air.local>
2023-03-16 01:46:33 -0400

    added index.html, and hyperlink and appended to page

---
+++ app.js
@@ -0,0 +1,5 @@
+let a = document.createElement('a');
+a.href = 'http://github.com/Epaiva14/my-favorites-list';
+a.textContent = 'Favorite movies and things in my closet!';
+
+document.body.appendChild(a);
